770f87e1ce68c009c143dcdd24ee5a13c5044c93
NetworkCrab <network123crab@gmail.com>
2023-05-21 00:02:13 +0530

    after doing the string method and more to go

---
+++ string.py
@@ -0,0 +1,26 @@
+# Defining a basic string
+sqt = "this is a demo string"
+print(sqt)
+
+# If we want to slice the string we will use
+print("We can slice the string like this -> ", sqt[0:3]) # The slicing will be done in the basis of 0 to 2 (3-1)
+
+# This will return the string in upper case
+print(sqt.upper())
+
+# This helps to make the first letter into uppercase
+print(sqt.capitalize())
+
+# To count the character or string repeated in the given string is been done through
+print("The count is -> ", sqt.count("i"))
+
+# The string is numeric or not ?
+number = "9330604675"
+print("This will check whether the string is numeric or not ? ", number.isnumeric())
+
+# print(number.isspace())
+
+
+
+
+
