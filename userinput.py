297e75c76ad1502f9c9819b5ed432bdfd864a2d8
NetworkCrab <network123crab@gmail.com>
2023-05-21 00:15:15 +0530

    git status

---
+++ userinput.py
@@ -0,0 +1,18 @@
+# Here we can learn about the userinput taken in python
+name = input("Enter your name here: ")
+print("Thanks for entering your name: ", name)
+
+# Write a python program from the user as an input and add 6 to it
+# We are using typecasting to do the following task!
+number = int(input("Enter the number: "))
+print("After adding 6 to the given number: ", number + 6)
+
+# To see the type of the existing number variable we will use
+print(type(number))
+
+
+
+
+
+
+
