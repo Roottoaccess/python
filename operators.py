01acd5189fdd131df29eb4148c842ac9ec1247c0
NetworkCrab <network123crab@gmail.com>
2023-05-20 21:17:38 +0530

    after completing the arithmetic operations

---
+++ operators.py
@@ -0,0 +1,11 @@
+# Here we are going to see the different operators in python
+number = 15.8
+number1 = 18.8
+print("The number = 15.8")
+print("The number1 = 18.8")
+# Performing arithmetic operations
+print("Performing addition = ", number + number1)
+print("Performing subtraction = ", number - number1)
+print("Performing multiply = ", number * number1)
+print("Performing divide = ", number / number1)
+print("Performing modules = ", number % number1)
