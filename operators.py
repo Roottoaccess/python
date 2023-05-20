d2046185e4c1d969447eb0be47eaa7b6fb62dd03
NetworkCrab <network123crab@gmail.com>
2023-05-20 22:30:36 +0530

    after completing the assignment operator

--- operators.py
+++ operators.py
@@ -1,6 +1,8 @@
 # Here we are going to see the different operators in python
 number = 15.8
 number1 = 18.8
+x = 2
+y = 5
 print("The number = 15.8")
 print("The number1 = 18.8")
 # Performing arithmetic operations
@@ -9,3 +11,9 @@ print("Performing subtraction = ", number - number1)
 print("Performing multiply = ", number * number1)
 print("Performing divide = ", number / number1)
 print("Performing modules = ", number % number1)
+print("Performing x to the power y = ", y ** x)
+print("Performing // means that number // number1 = ", number // number1)
+
+# Assignment operator
+a = 5
+print("Here a is the assignment operator ", a)
