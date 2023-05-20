311d6d36cbe7436691554b65f1f34a71a98aa961
NetworkCrab <network123crab@gmail.com>
2023-05-20 20:56:24 +0530

    after completing the variables and learning about the typecasting

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ print("hello world")
 
 # Variables in python
 number = 4
-print(number)
+print("This is an integer variable -> ", number)
 number1 = 55
 print(number1)
+number = 4.8
+print("This is a float variable -> ", number)
+boole = True
+print("Python can also support the boolien value -> ", boole)
+# In python we can use string also
+stri = "I am a python developer"
+print(stri)
+# Changing a string to an integer and then compute it
+# We did it with the help of typecasting
+f = "5"
+print(int(f) + 1)
