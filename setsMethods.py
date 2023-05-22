eee2bbe3d33567392293b5f55ee48610b8322468
NetworkCrab <network123crab@gmail.com>
2023-05-22 08:37:52 +0530

    after  completing understanding the set

---
+++ setsMethods.py
@@ -0,0 +1,19 @@
+# Learning about sets and sets methods
+# Building a set
+# In set an element will consider once no matter how much you repeat the element
+s1 = {4, 5, 25, 25, 25, 25}
+s2 = {4, 6, 24, 8, 108, 208}
+# s2.clear()
+# print(s2)
+# print(s1.pop())
+# s1.add(2)
+
+print(s1)
+print(s2)
+
+# Union function
+print("s2 union of s1:", s2.union(s1))
+
+# intersection function
+print("s2 intersect of s1:", s2.intersection(s1))
+
